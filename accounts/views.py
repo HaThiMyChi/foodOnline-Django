11899afb1a79fbf06c487fb49773ed22a54e0ce6
MyChi <mychikhtn@gmail.com>
2024-07-01 18:01:06 +0700

    User Registration Form Implementation

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http.response import HttpResponse
+from .forms import UserForm
+from .models import User
 
 
 # Create your views here.
 
 def registerUser(request):
-    return render(request, 'accounts/registerUser.html')
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            # form.save()
+            user.save()
+            
+            return redirect('registerUser')
+    else:
+
+        form = UserForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
