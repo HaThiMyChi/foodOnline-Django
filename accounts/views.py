cae9c22daf53251f6637986411a3a214e0ee3413
MyChi <mychikhtn@gmail.com>
2024-07-02 17:32:52 +0700

    Vendor Registration Template

--- accounts/views.py
+++ accounts/views.py
@@ -47,3 +47,6 @@ def registerUser(request):
         'form': form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+    return render(request, 'accounts/registerVendor.html')
